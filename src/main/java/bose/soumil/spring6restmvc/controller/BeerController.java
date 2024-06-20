e66f86136c2b0517b30e2b09fb0cc4b3dfd4d71c
soumil0507 <soumil0507@gmail.com>
2024-06-20 12:58:26 +0530

    Implement Logger using @Slf4j

--- src/main/java/bose/soumil/spring6restmvc/controller/BeerController.java
+++ src/main/java/bose/soumil/spring6restmvc/controller/BeerController.java
@@ -1,14 +1,23 @@
 package bose.soumil.spring6restmvc.controller;
 
+import bose.soumil.spring6restmvc.model.Beer;
 import bose.soumil.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 
     private final BeerService beerService;
 
+    public Beer getBeerById(UUID id){
+        log.debug("Get Beer by id - in controller");
+        return beerService.getBeerById(id);
+    }
 
 }
--- src/main/java/bose/soumil/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/bose/soumil/spring6restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,23 @@ package bose.soumil.spring6restmvc.services;
 
 import bose.soumil.spring6restmvc.model.Beer;
 import bose.soumil.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+//adding logger annotation
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
+
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by id - in service. Id {}", id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
